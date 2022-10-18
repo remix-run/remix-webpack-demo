42f57a66d00a4217ce28e54867709694dc567117
Pedro Cattori <pcattori@gmail.com>
2022-10-18 10:06:59 -0500

    👉 Copy/Paste the tutorial data module

---
+++ src/contact.ts
@@ -0,0 +1,80 @@
+import localforage from "localforage";
+import { matchSorter } from "match-sorter";
+import sortBy from "sort-by";
+
+type Contact = {
+  id: string;
+  createdAt: number;
+  first?: string;
+  last?: string;
+};
+
+export async function getContacts(query?: string) {
+  await fakeNetwork(`getContacts:${query}`);
+  let contacts = await localforage.getItem<Contact[]>("contacts");
+  if (!contacts) contacts = [];
+  if (query) {
+    contacts = matchSorter(contacts, query, { keys: ["first", "last"] });
+  }
+  return contacts.sort(sortBy("last", "createdAt"));
+}
+
+export async function createContact() {
+  await fakeNetwork();
+  let id = Math.random().toString(36).substring(2, 9);
+  let contact = { id, createdAt: Date.now() };
+  let contacts = await getContacts();
+  contacts.unshift(contact);
+  await set(contacts);
+  return contact;
+}
+
+export async function getContact(id: string) {
+  await fakeNetwork(`contact:${id}`);
+  let contacts = (await localforage.getItem<Contact[]>("contacts"))!;
+  let contact = contacts.find((contact) => contact.id === id);
+  return contact ?? null;
+}
+
+export async function updateContact(id: string, updates: Partial<Contact>) {
+  await fakeNetwork();
+  let contacts = (await localforage.getItem<Contact[]>("contacts"))!;
+  let contact = contacts.find((contact) => contact.id === id);
+  if (!contact) throw new Error(`No contact found for ${id}`);
+  Object.assign(contact, updates);
+  await set(contacts);
+  return contact;
+}
+
+export async function deleteContact(id: string) {
+  let contacts = (await localforage.getItem<Contact[]>("contacts"))!;
+  let index = contacts.findIndex((contact) => contact.id === id);
+  if (index > -1) {
+    contacts.splice(index, 1);
+    await set(contacts);
+    return true;
+  }
+  return false;
+}
+
+function set(contacts: Contact[]) {
+  return localforage.setItem("contacts", contacts);
+}
+
+// fake a cache so we don't slow down stuff we've already seen
+let fakeCache: Record<string, boolean> = {};
+
+async function fakeNetwork(key?: string) {
+  if (!key) {
+    fakeCache = {};
+    return;
+  }
+  if (fakeCache[key]) {
+    return;
+  }
+
+  fakeCache[key] = true;
+  return new Promise((res) => {
+    setTimeout(res, Math.random() * 800);
+  });
+}
---
+++ src/sort-by.d.ts
@@ -0,0 +1,6 @@
+declare module "sort-by" {
+  type CompareFn<T> = (a: T, b: T) => number;
+  export default function sortBy<T extends Record<PropertyKey, unknown>>(
+    ...keys: (keyof T | `-${keyof T}`)[]
+  ): CompareFn<T>;
+}
