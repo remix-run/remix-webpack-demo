0d2bfb736867d2e590da4052e3c52dc87fe2afee
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:06:43 -0500

    👉 Create the action and change <form> to <Form>

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,13 @@
-import { Link, Outlet, useLoaderData } from "react-router-dom";
-import { Contact, getContacts } from "../contact";
+import { Form, Link, Outlet, useLoaderData } from "react-router-dom";
+import { Contact, getContacts, createContact } from "../contact";
 
 export async function loader() {
   const contacts = await getContacts();
   return { contacts };
+}
+
+export async function action() {
+  await createContact();
 }
 
 export default function Root() {
@@ -31,9 +35,9 @@ export default function Root() {
               aria-live="polite"
             ></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
