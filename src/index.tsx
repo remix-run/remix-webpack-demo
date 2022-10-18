ceab889c37a7d581e040c283081c10503cbcd2e4
Pedro Cattori <pcattori@gmail.com>
2022-10-18 10:27:57 -0500

    👉 Set the <ErrorPage> as the errorElement on the root route

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
