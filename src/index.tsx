58d916d426cb15cdd7d2859756cf384d1be8d14a
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:26:41 -0500

    👉 Configure the loader on the route

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {
 import "./index.css";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
