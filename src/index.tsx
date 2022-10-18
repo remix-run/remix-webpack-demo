c1b356aefdf8fa770e37bac815842a94b0036838
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:58:40 -0500

    👉 Configure the index route

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
