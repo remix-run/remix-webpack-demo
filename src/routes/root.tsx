0fc2afaa153a1d0ed78f096649f92181ac8e7d06
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:50:16 -0500

    👉 Use a NavLink in the sidebar

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Form, NavLink, Outlet, redirect, useLoaderData } from "react-router-dom";
 import { Contact, getContacts, createContact } from "../contact";
 
 export async function loader() {
@@ -45,7 +45,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -54,7 +63,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
