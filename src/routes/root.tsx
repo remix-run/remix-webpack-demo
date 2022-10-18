f1ee7ddae83432cde19404d2bff2db495adabd08
Pedro Cattori <pcattori@gmail.com>
2022-10-18 10:59:13 -0500

    👉 Export a loader from root.jsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
+import { getContacts } from "../contact";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 
 export default function Root() {
   return (
