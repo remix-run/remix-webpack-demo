e9c2a7ee2f534a6d77b19930c981fce0496d6588
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:34:21 -0500

    👉 Wire the action up to the route

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import "./index.css";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
-import EditContact from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
