67bb0c0385e1219e7daee12e1bb8b0b42e1f4ed6
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:56:46 -0500

    https://reactrouter.com/en/main/start/tutorial#contextual-errors

--- src/index.tsx
+++ src/index.tsx
@@ -33,6 +33,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
