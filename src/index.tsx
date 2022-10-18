420d0f4714aae5778f4c051462b55029395a6aec
Pedro Cattori <pcattori@gmail.com>
2022-10-18 11:07:28 -0500

    👉 Import and set the action on the route

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
