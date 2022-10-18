a31b9af62fa8034e48bb9fc108e9949a0e8df31b
Pedro Cattori <pcattori@gmail.com>
2022-10-18 13:36:57 -0500

    👉 Use replace in submit

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -53,7 +53,10 @@ export default function Root() {
               name="q"
               defaultValue={q}
               onChange={(event) => {
-                submit(event.currentTarget.form);
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
               }}
             />
             <div
